d17c103e156d47d15b6f1c1977e80115c1b03b0c
Leandro Silva <leandrosilva7.contato@gmail.com>
2026-03-23 11:35:45 -0300

    Delete option

--- editarAluno.php
+++ editarAluno.php
@@ -17,10 +17,60 @@ if(isset($_POST["editar"])) {
     $nome = $_POST["nome"];
     $idade = $_POST["idade"];
 
-    $linhas = file("alunos.txt");
-    $linhas[$id] = "$nome;$idade;\n";
+    if(file_exists("alunos.txt")) {
+        $arq = fopen("alunos.txt", "r");
+        $novoConteudo = "";
+        $linhaAtual = 0;
 
-    file_put_contents("alunos.txt", implode("", $linhas));
+        while(!feof($arq)) {
+            $linha = fgets($arq);
+            if($linha === false) break;
+
+            if($linhaAtual == $id) {
+                $linha = "$nome;$idade;\n"; // substitui a linha
+            }
+
+            $novoConteudo .= $linha;
+            $linhaAtual++;
+        }
+
+        fclose($arq);
+
+        // Reescreve o arquivo
+        $arq = fopen("alunos.txt", "w");
+        fwrite($arq, $novoConteudo);
+        fclose($arq);
+    }
+}
+
+// EXCLUIR
+if(isset($_POST["excluir"])) {
+    $idExcluir = $_POST["id"];
+    $novoConteudo = "";
+    $linhaAtual = 0;
+
+    if(file_exists("alunos.txt")) {
+        $arq = fopen("alunos.txt", "r");
+
+        while(!feof($arq)) {
+            $linha = fgets($arq);
+
+            if($linhaAtual != $idExcluir) {
+                $novoConteudo .= $linha;
+            }
+
+            $linhaAtual++;
+        }
+
+        fclose($arq);
+
+        $arq = fopen("alunos.txt", "w");
+        fwrite($arq, $novoConteudo);
+        fclose($arq);
+    }
+    
+    header("Location: ".$_SERVER['PHP_SELF']);
+    exit;
 }
 
 // CADASTRAR (só se NÃO estiver editando)
@@ -68,31 +118,37 @@ if(isset($_POST["mostrar"]) || isset($_POST["carregar"]) || isset($_POST["editar
 
     if(file_exists("alunos.txt")) {
 
-        $linhas = file("alunos.txt");
+        $arq = fopen("alunos.txt", "r");
 
         echo "<table border='1'>";
         echo "<tr><th>Nome</th><th>Idade</th><th>Ação</th></tr>";
 
-        foreach($linhas as $index => $linha) {
-            if(trim($linha) != "") {
-                $dados = explode(";", $linha);
+        $index = 0;
+        while(!feof($arq)) {
+            $linha = fgets($arq);
+            if($linha === false) break;
+            if(trim($linha) == "") continue;
 
-                echo "<tr>";
-                echo "<td>".$dados[0]."</td>";
-                echo "<td>".$dados[1]."</td>";
+            $dados = explode(";", $linha);
 
-                echo "<td>
-                    <form method='POST' style='display:inline'>
-                        <input type='hidden' name='id' value='$index'>
-                        <input type='hidden' name='nome' value='{$dados[0]}'>
-                        <input type='hidden' name='idade' value='{$dados[1]}'>
-                        <button name='carregar'>Editar</button>
-                    </form>
-                </td>";
+            echo "<tr>";
+            echo "<td>".$dados[0]."</td>";
+            echo "<td>".$dados[1]."</td>";
+            echo "<td>
+                <form method='POST' style='display:inline'>
+                    <input type='hidden' name='id' value='$index'>
+                    <input type='hidden' name='nome' value='{$dados[0]}'>
+                    <input type='hidden' name='idade' value='{$dados[1]}'>
+                    <button name='carregar'>Editar</button>
+                    <button name='excluir'>Excluir</button>
+                </form>
+            </td>";
+            echo "</tr>";
 
-                echo "</tr>";
-            }
+            $index++;
         }
+
+        fclose($arq);
 
         echo "</table>";
     }
